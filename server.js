92cafc059d785dd1210d7c3536448d5ec04af301
AysanurKart <ayka21ab@student.cbs.dk>
2022-07-27 15:18:03 +0200

    created port

---
+++ server.js
@@ -0,0 +1,8 @@
+const { Console } = require("console");
+const express = require("express")
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is listening on http://localholst:${PORT}`)
+});
